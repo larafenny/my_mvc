7578aa3da85bdd03ab72a03b4e4e078bfdc81c39
Lara Filippone <lara.filippone@gmail.com>
2023-10-22 18:54:11 +0200

    add DB methods to make query, fetch data

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,14 +2,22 @@
 
     class Pages extends Controller
     {
+        public $postModel;
+
         public function __construct() {
+            $this->postModel = $this->model('post');
         }
 
         public function index() {
+
+
+            $posts = $this->postModel->getPosts();
+
             $data = [
-                'title' => 'HOMEPAGE',
-                'subtitle' => 'benvenuti'
+                'title' => 'ciao',
+                'posts' => $posts,
             ];
+
             $this->view('pages/index', $data);
         }
 
--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -27,4 +27,51 @@ class Database
             echo $this->error;
         }
     }
+
+    // prepare statement with query
+    public function query($sql) {
+        $this->statement = $this->dbHandler->prepare($sql);
+    }
+
+    // method to bind values
+    public function bind($param, $value, $type = null) {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->statement->bindValue($param, $value, $type);
+    }
+
+    // execute the prepare statement
+    public function execute() {
+        return $this->statement->execute();
+    }
+
+    // get results set as array of objects
+    public function resultSet() {
+        $this->execute();
+        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    // get single record as object
+    public function single() {
+        $this->execute();
+        return $this->statement->fetch(PDO::FETCH_OBJ);
+    }
+
+    // get row count
+    public function rowCount() {
+        return $this->statement->rowCount();
+    }
 }
