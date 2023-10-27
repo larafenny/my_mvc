673a38d800ba3b91dc1ef75b2a963a3796d63a9a
Lara Filippone <lara.filippone@gmail.com>
2023-10-27 18:48:23 +0200

    clean up + documentation

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,10 @@
 <?php
     // DB params
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'myMvc');
-    define('DB_PASS', 'myMvc123');
-    define('DB_NAME', 'mymvc');
+    define('DB_USER', '_YOUR_USER_');
+    define('DB_PASS', '_YOUR_PASS_');
+    define('DB_NAME', '_YOUR_DB_NAME_');
 
     define('APPROOT', dirname(dirname(__FILE__)));
-    define('URLROOT', 'http://localhost:8888/my_mvc');
-    define('SITENAME', 'MY MVC');
+    define('URLROOT', '_YOUR_URL_');
+    define('SITENAME', '_YOUR_SITE_NAME_');
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,17 +5,12 @@
         public $postModel;
 
         public function __construct() {
-            $this->postModel = $this->model('post');
+
         }
 
         public function index() {
-
-
-            $posts = $this->postModel->getPosts();
-
             $data = [
                 'title' => 'ciao',
-                'posts' => $posts,
             ];
 
             $this->view('pages/index', $data);
